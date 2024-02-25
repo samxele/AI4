ce7217cb527a95003b53a47d16d169653f678747
ishaanthenerd <145402353+ishaanthenerd@users.noreply.github.com>
2024-02-25 04:16:35 +0000

    Fixed the move() function in game.py

--- game.py
+++ game.py
@@ -27,36 +27,47 @@ class Game:
     '''
     def move(self, picked_column):
         
-        
         if picked_column < 0 or picked_column > 6:
-            # Print or return a value for invalid input
-            pass
+            # print or return a value for invalid input
+            print("Invalid column, enter again")
+            return
             
         row = 5
         
-        # Check 
-        while row >= 0 and self.board[0][row][picked_column] != 1:
+        # check 
+        while row >= 0 and self.board[0][row][picked_column] == 0:
             row -= 1
             
         if row == -1:
-            # column full
-            # Print column full
+            # print column full
+            print("Column is full, enter another column")
             return
         
         self.board[0][row][picked_column] = 0
         self.board[self.turn][row][picked_column] = 1
+        self.column = picked_column
+        
+        # check for win
+        
+        self.win()
+        
+        # turn change
+        if self.turn == 1:
+            self.turn = 2
+        else:
+            self.turn = 1
     '''
     --> End of code that needs to be fixed
     '''
 
     def win(self):
-        # flips the turn (if it's P1's turn, P2 moved last)
-        turn = 3 - self.turn
+        # checks the board for the person who just made a move
+        turn = self.turn
         
         # the highest possible is at row 0
         highest = 0
         # iterate while the piece is there
-        while (self.board[turn][highest][self.column] == 0):
+        while self.board[turn][highest][self.column] == 0:
             highest += 1
         
         # check whether it works
@@ -67,59 +78,59 @@ class Game:
         squares_right = 7 - self.column
         
         # checks down
-        if (squares_down >= 4):
+        if squares_down >= 4:
             square_sum = 0
             for i in range(0, 4):
                 square_sum += self.board[turn][highest + i][self.column]
-            if (square_sum == 4):
+            if square_sum == 4:
                 has_won = True
         
         # checks left
-        if (squares_left >= 4):
+        if squares_left >= 4:
             square_sum = 0
             for i in range(0, 4):
                 square_sum += self.board[turn][highest][self.column - i]
-            if (square_sum == 4):
+            if square_sum == 4:
                 has_won = True
         
         # checks right
-        if (squares_right >= 4):
+        if squares_right >= 4:
             square_sum = 0
             for i in range(0, 4):
                 square_sum += self.board[turn][highest][self.column + i]
-            if (square_sum == 4):
+            if square_sum == 4:
                 has_won = True
         
         # checks top left
-        if (squares_up >= 4 and squares_left >= 4):
+        if squares_up >= 4 and squares_left >= 4:
             square_sum = 0
             for i in range(0, 4):
                 square_sum += self.board[turn][highest - i][self.column - i]
-            if (square_sum == 4):
+            if square_sum == 4:
                 has_won = True
         
         # checks bottom right
-        if (squares_down >= 4 and squares_right >= 4):
+        if squares_down >= 4 and squares_right >= 4:
             square_sum = 0
             for i in range(0, 4):
                 square_sum += self.board[turn][highest + i][self.column + i]
-            if (square_sum == 4):
+            if square_sum == 4:
                 has_won = True
         
         # checks top right 
-        if (squares_up >= 4 and squares_right >= 4):
+        if squares_up >= 4 and squares_right >= 4:
             square_sum = 0
             for i in range(0, 4):
                 square_sum += self.board[turn][highest - i][self.column + i]
-            if (square_sum == 4):
+            if square_sum == 4:
                 has_won = True
         
         # checks bottom left
-        if (squares_down >= 4 and squares_left >= 4):
+        if squares_down >= 4 and squares_left >= 4:
             square_sum = 0
             for i in range(0, 4):
                 square_sum += self.board[turn][highest + i][self.column - i]
-            if (square_sum == 4):
+            if square_sum == 4:
                 has_won = True
         
         # modify the state based on boolean
@@ -129,5 +140,5 @@ class Game:
             empty_sum = 0
             for col in range(7):
                 empty_sum += self.board[0][0][col]
-            if (empty_sum == 7):
+            if empty_sum == 7:
                 self.state = 0
