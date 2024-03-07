3d4d92d19fa2a213d611947e3fb5e6a9f7897bf1
ishaanthenerd <145402353+ishaanthenerd@users.noreply.github.com>
2024-03-07 03:56:51 +0000

    Debugged part of the minimax function

--- game.py
+++ game.py
@@ -129,14 +129,17 @@ class Game:
             empty_sum = 0
             for col in range(7):
                 empty_sum += self.board[0][0][col]
-            if empty_sum == 7:
+            if empty_sum == 0:
                 self.state = 0
+
+    def game_minimax(self, depth):
+        return minimax(self.board, self.turn, depth)
 
     def playGame(self):
     # play game until either player wins or game draws
         while (self.state == -1):
-            # make best move for current player
-            self.game_move(self.board, heuristics.minimax(self.board, self.player, 4)[1])
+            # make best move for current player for depth 4
+            self.game_move(self.board, self.game_minimax(4)[1])
 
 def move(board, picked_column, turn):
         
@@ -146,7 +149,6 @@ def move(board, picked_column, turn):
             return
             
         row = 5
-        
         # check 
         while row >= 0 and board[0][row][picked_column] == 0:
             row -= 1
@@ -158,3 +160,39 @@ def move(board, picked_column, turn):
         
         board[0][row][picked_column] = 0
         board[turn][row][picked_column] = 1
+
+def minimax(board, player, depth):
+    # goal is for P1 to maximize score and for P2 to minimize score
+    # valid scores can be between -1 and 1
+    if depth == 0:
+        return heuristics.static_evaluation(board, player)
+    elif player == 1:
+        # player 1 wants to do MUCH better than -1 (maximize score)
+        maxEval = -1
+        best_move = -1
+        for i in range(7):
+            mod_board = board
+            mod_board.move(i)
+            # use new board, switch player's turn, and go deeper
+            eval = minimax(mod_board, 2, depth - 1)[0]
+            if eval >= maxEval:
+                maxEval = eval
+                best_move = i
+        return (maxEval, best_move)
+    elif player == 2:
+        # player 2 wants to do MUCH better than 1 (minimize score)
+        minEval = 1
+        best_move = -1
+        for i in range(7):
+            mod_board = board
+            mod_board.move(i)
+            # use new board, switch player's turn, and go deeper
+            eval = minimax(mod_board, 1, depth - 1)[0]
+            if eval <= minEval:
+                minEval = eval
+                best_move = i
+        return (minEval, best_move)
+    else:
+        # error checking for invalid player number
+        print("Invaild player!")
+        return
--- heuristics.py
+++ heuristics.py
@@ -1,50 +1,9 @@
 import numpy as np
 import game
 
+# sigmoid function
 def scaled_sigmoid(x):
     return 2 / (1 + np.exp(-x)) - 1
-
-def minimax(board, player, depth):
-    # goal is for P1 to maximize score and for P2 to minimize score
-    # valid scores can be between -1 and 1
-    if board.state == 1:
-        # player 1 has won, score of position is 1
-        return (1, -1)
-    elif board.state == 2:
-        # player 2 has won, score of position is -1
-        return (-1, -1)
-    elif depth == 0:
-        return static_evaluation(board, player)
-    elif player == 1:
-        # player 1 wants to do MUCH better than -1 (maximize score)
-        maxEval = -1
-        best_move = -1
-        for i in range(7):
-            mod_board = board
-            mod_board.move(i)
-            # use new board, switch player's turn, and go deeper
-            eval = minimax(mod_board, 2, depth - 1)[0]
-            if eval >= maxEval:
-                maxEval = eval
-                best_move = i
-        return (maxEval, best_move)
-    elif player == 2:
-        # player 2 wants to do MUCH better than 1 (minimize score)
-        minEval = 1
-        best_move = -1
-        for i in range(7):
-            mod_board = board
-            mod_board.move(i)
-            # use new board, switch player's turn, and go deeper
-            eval = minimax(mod_board, 1, depth - 1)[0]
-            if eval <= minEval:
-                minEval = eval
-                best_move = i
-        return (minEval, best_move)
-    else:
-        # error checking for invalid player number
-        print("Invaild player!")
-        return
 
 # static evaluation of position
 def static_evaluation(board, player):
